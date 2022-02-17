b6f2ea3aeac76dc131751c4db0bbda854b3ce2c2
elvis arias <elvisarias1995@gmail.com>
2022-02-17 18:23:09 -0300

    subiendo fichero 3

--- fichero_1.py
+++ fichero_1.py
@@ -1,6 +1,6 @@
 """ Escribir una función que pida un número entero entre 1 y 10 
 y guarde en un fichero con el nombre tabla-n.txt 
-la tabla de multiplicar de ese número, done n es el número introducido. """
+la tabla de multiplicar de ese número, done n es el número introducido  """
 
 def save_file(file_name, number):
     with open(file_name, 'w') as file:
---
+++ fichero_3.py
@@ -0,0 +1,14 @@
+from io import open
+
+def main():
+    file = open("tabla-5.txt", 'r')
+    line = file.readlines()
+    
+    for item in line:
+        print(item)
+        # obteniendo solo un el resultado
+        print(item[8:].lstrip())
+    
+
+if __name__ == "__main__":
+    main()
